c406f8f767ea14eedc8c7f9064176ba51340afb5
fiademm <fiademm1@gmail.com>
2023-06-07 11:11:56 +0000

    Update courses file

--- routes/courses.js
+++ routes/courses.js
@@ -16,27 +16,51 @@ router.get('/', async (req, res) => {
     }
 });
 
-// get a course by id
 router.get('/:id', async (req, res) => {
     try {
-        const courseId = parseInt(req.params.id);
-        const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-        const result = await pool.query('SELECT * FROM courses WHERE id = $1', [courseId]);
-        const course = result.rows[0];
-        const result2 = await pool.query('SELECT enrolled_courses FROM users WHERE id=$1', [userId]);
-        const enrolledCourses = result2.rows[0].enrolledCourses || [];
-
-        if (!enrolledCourses.includes(courseId)) {
-            return res.status(401).send('You are not enrolled in this course');
-        }
-        const result3 = await pool.query('SELECT progress FROM course_progress WHERE user_id = $1 AND course_id = $2', [userId, courseId]);
-        const progress = result3.rows[0]?.progress || 0;
-        res.json({ ...course, progress });
+      const courseId = parseInt(req.params.id);
+      const token = req.headers.authorization.split(' ')[1];
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      // const userId = decodedToken.sub;
+      const userId = decodedToken.userId;
+      const result = await pool.query('SELECT * FROM courses WHERE id = $1', [courseId]);
+      const course = result.rows[0];
+      const result2 = await pool.query('SELECT enrolled_courses FROM users WHERE id = $1', [userId]);
+      console.log('This is the user id ' + userId);
+      const enrolledCourses = result2.rows[0]?.enrolled_courses ?? [];
+      //const enrolledCourses = result2.rows[0].enrolled_courses || [];
+      if (!enrolledCourses.includes(courseId)) {
+        return res.status(401).send('You are not enrolled in this course');
+      }
+      const result3 = await pool.query('SELECT progress FROM course_progress WHERE user_id = $1 AND course_id = $2', [userId, courseId]);
+      const progress = result3.rows[0]?.progress || 0;
+      res.json({ ...course, progress });
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Server error');
+      console.error(error);
+      res.status(500).send('Server error');
     }
-});
+  });
+
+// // Get a course by ID
+// router.get('/:id', async (req, res) => {
+//     try {
+//       const courseId = parseInt(req.params.id);
+//       const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+//       const result = await pool.query('SELECT * FROM courses WHERE id = $1', [courseId]);
+//       const course = result.rows[0];
+//       const result2 = await pool.query('SELECT enrolled_courses FROM users WHERE id = $1', [userId]);
+//       const enrolledCourses = result2.rows[0].enrolled_courses || [];
+//          if (!enrolledCourses.includes(courseId)) {
+//         return res.status(401).send('You are not enrolled in this course');
+//       }
+//       const result3 = await pool.query('SELECT progress FROM course_progress WHERE user_id = $1 AND course_id = $2', [userId, courseId]);
+//       const progress = result3.rows[0]?.progress || 0;
+//       res.json({ ...course, progress });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send('Server error');
+//     }
+//   });
 
 // update a user's progress for a course
 router.post('/:id/progress', async (req, res) => {
